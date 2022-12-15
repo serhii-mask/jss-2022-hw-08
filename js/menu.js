f79984ba009caa0bb2c1c1a4f18d851bf4b3b54c
Serhii <serhii.mask@gmail.com>
2022-12-15 20:45:55 +0200

    onscrol

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,20 @@
 (() => {
    const menuBtnRef = document.querySelector("[data-menu-button]");
    const mobileMenuRef = document.querySelector("[data-menu]");
+   const bodyOnscrol = document.querySelector("[data-body]");
+  //  const bodyOnscrol = document.querySelector("[data-body]");
  
    menuBtnRef.addEventListener("click", () => {
      const expanded =
        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
- 
+
      menuBtnRef.classList.toggle("is-open");
      menuBtnRef.setAttribute("aria-expanded", !expanded);
  
      mobileMenuRef.classList.toggle("is-open");
+
+     bodyOnscrol.classList.toggle("is-open");
+    //  bodyOnscrol.classList.toggle("is-open");
+ 
    });
  })();
